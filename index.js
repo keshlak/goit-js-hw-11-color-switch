91e38a2faabefb0c41c68304e4bf097940bb55bc
keshlak <cithreepio@gmail.com>
2021-05-17 17:38:00 +0300

    smth

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 refs = {
   startBtn: document.querySelector("[start]"),
   stopBtn: document.querySelector("[stop]"),
+  body: document.querySelector("body"),
   colors: ["#FFFFFF", "#2196F3", "#4CAF50", "#FF9800", "#009688", "#795548"],
 };
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-
-refs.startBtn = addEventListener("click", () => {});
+const colorSwitch = function () {};
+refs.startBtn = addEventListener("click", () => {
+  refs.body.style.backgroundColor = refs.colors[2];
+});
 
 refs.stopBtn = addEventListener("click", () => {});
